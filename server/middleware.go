c2a5411ae9aa0223b7f87f621ccdee2636b3de98
mikloslorinczi <miklos.lorinczi@bitrise.io>
2018-11-13 15:01:20 +0100

    unauth troubleshoot added

--- server/middleware.go
+++ server/middleware.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/spf13/viper"
@@ -10,7 +11,11 @@ import (
 // in the Header, and if its value is equals to the one set by Viper.
 func authCheck(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
-		if req.Header.Get("MOTHLAMP_API_TOKEN") != viper.GetString("MOTHLAMP_API_TOKEN") {
+		for key, value := range req.Header {
+			fmt.Printf("Incoming HEADER %v : %v\n", key, value)
+		}
+		if req.Header.Get("apitoken") != viper.GetString("MOTHLAMP_API_TOKEN") {
+			fmt.Printf("Token missmatch! Expected : %v Actual : %v\n", viper.GetString("MOTHLAMP_API_TOKEN"), req.Header.Get("apitoken"))
 			res.WriteHeader(http.StatusUnauthorized)
 			res.Write([]byte("Unauthorized\n"))
 		} else {
